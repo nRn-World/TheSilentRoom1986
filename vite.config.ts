5af506efaa87b38ffcbb4d31c1d23b767246f806
Hello World! <132290183+nRn-World@users.noreply.github.com>
2026-08-28 15:09:02 +0200

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/The-Silent-Room-1986/',
+    base: '/TheSilentRoom1986/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
